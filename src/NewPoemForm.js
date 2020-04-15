71e4186cd00b987ea204bea2acce230b3666a481
Mitchell <mitchell.alton@live.com>
2020-04-15 11:05:09 -0500

    finished mostly

--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -2,8 +2,9 @@ import React from "react";
 
 class NewPoemForm extends React.Component {
   render() {
+    // {this.props.newPoem()}
     return (
-      <form className="new-poem-form">
+      <form onSubmit={(e) => this.props.newPoem(e)}className="new-poem-form">
         <input placeholder="Title" />
         <input placeholder="Author" />
         <textarea placeholder="Write your masterpiece here..." rows={10} />
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -3,10 +3,14 @@ import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
   render() {
+  // console.log(this.props.poems)
+
     return (
+
       <div className="poems-container">
+        <h1>All Poems</h1>
         {
-          // render poems here
+          this.props.poems ? this.props.poems.map(poem => <Poem poem={poem} addToFav={this.props.addToFav} deletePoem={this.props.deletePoem}/>) : null
         }
       </div>
     );
